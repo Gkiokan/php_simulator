5501956a5ae9dd005c3948ae865ce4d0c930df1c
Gkiokan <gkiokan@hotmail.de>
2016-02-21 23:35:12 +0100

    Using ace for Code Editing Modified the Core Object and generating all needen stuff which is needen automatically for now. There will be a better constructor later on. Black Theme swapped.

--- assets/js/simulator-core.js
+++ assets/js/simulator-core.js
@@ -8,21 +8,36 @@
 
 
 // Just getting the source out, i'll make an awsome Object for this later.
-$(function(){
-    function debug(new_debug){
+function php_simulator(){
+
+    // Some preparations
+    var timer  = 500;
+    var editor = {};
+    var code   = {};
+
+    // Generate the Code Editor Views
+    this.generate_editor = function (){
+       editor.php = ace.edit("php");
+       editor.php.setTheme("ace/theme/twilight");
+       editor.php.session.setMode({path: "ace/mode/php", pure:true, inline:true});
+    }
+
+    // Debug propose?
+    this.debug = function (new_debug){
         var debug = $('.debug');
         var old_debug = $('.debug').html();
         //var new_debug = old_debug+'+';
         debug.html(new_debug);
     }
 
-    function get_code(){
-        var code = {}
-        code.php = $('.key').val();
+    // Get the Source Code and return it
+    this.get_code = function (){
+        code.php = editor.php.getValue(); // $('.key').val();
         return code;
     }
 
-    function push_code(code){
+    // Get the Code and execute it
+    this.push_code = function (code){
         $.post('core/sim.php', { code:code }, function(data) {
             var result = $('.result');
             result.html(data);
@@ -30,12 +45,21 @@ $(function(){
 
     }
 
-    /* Animation Frame */
-    setInterval(function(){
-        code = get_code();
-        // debug(code);
-        push_code(code);
+    // Start Interval for code execution
+    this.start_interval = function(that) { var that = that; setInterval(function(){ that.push_code(that.get_code()) }, timer) }
 
-    }, 1000/60);
+    //
+    this.init = function(){
+      this.generate_editor();
+      this.start_interval(this);
+    }
 
-});
+};
+
+// We'll stick the Initialisation to this file for now,
+// I'll make a more powerfull constructor for this later on.
+$(function(){
+  var magic = new php_simulator();
+  console.log(magic);
+  magic.init();
+})
--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 <html>
     <head>
         <meta name='charset' content='utf-8'>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name='author' content='Gkiokan Sali'>
         <meta name='keywords' content=''>
         <meta name='description' content=''>
@@ -23,30 +24,41 @@
     <body>
     <div class='main'>
 
-    <div class='container simulator'>
-        <div class='row'>
-            <div class='col-xs-12'>
-                <h1>PHP Simulator</h1>
-            </div>
-            <div class='debug'></div>
-        </div>
-        <div class='row'>
-            <div class='col-xs-6'>
-                <h3>Source Code</h3>
-                <div class='input'>
-                <textarea class='key' id='key'></textarea>
+        <div class='simulator'>
+        <div class='container-fluid'>
+            <div class='row heading'>
+                <div class='col-xs-12'>
+                    <h1>PHP Simulator</h1>
+                    <div class='debug'></div>
                 </div>
             </div>
-            <div class='col-xs-6'>
-                <h3>Result</h3>
-                <div class='result'></div>
+            <div class='row settings'>
+                <div class='col-xs-12'>
+                    <div class='btn-group btn-group-xs'>
+                        <div class='btn btn-info'>Setup Request Timer</div>
+                        <div class='btn btn-success'>Code Theme </div>
+                        <div class='btn btn-danger'>Run Code </div>
+                    </div>
+                </div>
+            </div>
+            <div class='row code_sides'>
+                <div class='col-xs-6'>
+                    <h3>Source Code</h3>
+                    <div class='input' id='php'></div>
+                </div>
+                <div class='col-xs-6'>
+                    <h3>Result</h3>
+                    <div class='result'></div>
+                </div>
             </div>
         </div>
-    </div>
+        </div>
+
     </div>
 
     <script src='assets/js/jquery-1.11.1.min.js'></script>
     <script src='assets/js/bootstrap.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.3/ace.js'></script>
     <script src="assets/js/simulator-core.js"></script>
     </body>
 </html>
